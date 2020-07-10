7168fe6bcb1fc29c0073060a02fc61cc299ac5bd
ahkui <ahkui@outlook.com>
2020-07-10 18:15:53 +0800

    change powerline to airline

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 git submodule update --init --recursive
 vim +PluginInstall +qall
-cd ~/.vim/bundle/YouCompleteMe
-./install.sh
+sh -c "cd ~/.vim/bundle/YouCompleteMe && ./install.py"
 
