df885c589244e2864c6cf7a7c47ac54d98763ef7
ahkui <ahkui@outlook.com>
2021-01-10 16:04:35 +0800

    update package

--- install-YouCompleteMe.sh
+++ install-YouCompleteMe.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-sh -c "cd ~/.vim/bundle/YouCompleteMe && ./install.py"
-
+sh -c "cd ~/.vim/bundle/YouCompleteMe && git remote update && git reset --hard origin/master && git submodule update --init --recursive && python3 install.py --all"
